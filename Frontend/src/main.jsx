9e232a35685d86b091898df8c32f4f7c256b4ba2
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-03-11 23:37:08 +0600

    File-folder-create-done-frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import "./assets/css/bootstrap.css";
+import "./assets/css/animate.min.css";
+import "./assets/css/style.css";
+import "./assets/css/progress.css";
+import "./assets/css/dropdownmenu.css";
+import "./assets/css/sidebar.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
