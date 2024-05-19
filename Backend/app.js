060ed6e6871c5f7c023e59bedc3c4dfd5866bac3
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-05-19 11:59:46 +0600

    cors-remove

--- Backend/app.js
+++ Backend/app.js
@@ -17,13 +17,7 @@ const rateLimit = require("express-rate-limit");
 
 //middleware calling
 
-app.use(
-  cors({
-    origin: ["https://full-stack-task-manager-w2oq.vercel.app"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true, // If you're using cookies or sessions
-  })
-);
+app.use(cors());
 app.use(helmet());
 app.use(hpp());
 app.use(mongoSanitize());
