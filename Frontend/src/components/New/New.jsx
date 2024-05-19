c21519f2f266ac7329cd5bc7b733cd0c79196356
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-05-19 08:54:36 +0600

    new-file-solve

--- Frontend/src/components/New/New.jsx
+++ Frontend/src/components/New/New.jsx
@@ -5,7 +5,7 @@ import {
   AiOutlineDelete,
   AiOutlineEdit,
 } from "react-icons/ai";
-import { TaskListByStatus } from "../../APIRequest/APIRequest";
+import { TaskListByStatus } from "../../APIRequest/APIRequest.js";
 import { useSelector } from "react-redux";
 import { DeleteToDO } from "../../helper/DeleteAlert";
 import { UpdateToDO } from "../../helper/UpdateAlert";
