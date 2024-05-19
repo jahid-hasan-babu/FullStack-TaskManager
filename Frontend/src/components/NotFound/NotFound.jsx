4fd2e510dcd32773511c18b93e684badf9d9d4b9
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-05-19 08:04:19 +0600

    not-found-add

---
+++ Frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        className="w-100 h-auto"
+        src="https://sitechecker.pro/wp-content/uploads/2023/06/404-status-code.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default NotFound;
--- Frontend/src/page/Page404.jsx
+++ Frontend/src/page/Page404.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Fragment, Suspense } from "react";
+import MasterLayout from "../components/MasterLayout/MasterLayout";
+import LazyLoader from "../components/MasterLayout/LazyLoader";
+import NotFound from "../components/NotFound/NotFound";
 
 const Page404 = () => {
-  return <div></div>;
+  return (
+    <Fragment>
+      <MasterLayout>
+        <Suspense fallback={<LazyLoader />}>
+          <NotFound />
+        </Suspense>
+      </MasterLayout>
+    </Fragment>
+  );
 };
 
 export default Page404;
